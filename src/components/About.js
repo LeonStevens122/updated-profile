69d2fc9ccba09af783cbb7a85b488e21f9c17f1f
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-07-14 21:30:04 +0200

    Second Commit, changed alignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 // pages/profile.js
 
 import React from "react";
-import { Row, Col } from "react-materialize";
+import { Container, Row, Col } from "react-materialize";
 
 // about component exports the component
 const About = () => {
@@ -10,52 +10,60 @@ const About = () => {
       <div>
         <div className="row">
           <br />
-          <p className="education-title">
+          <p className="main-title">
             {" "}
             Hi, My name is Leon. Im a Full Stack Web Developer living in Cape
-            Town{" "}
+            Town.{" "}
           </p>
         </div>
-        <div className="row">
-          <br />
-          <p className="education-title">
-            {" "}
-            Full Stack Web Developer(MERN), HTML - CSS - JS - REACT - Express -
-            Node - Python - Django{" "}
-          </p>
-        </div>
-        <Row>
-          <Col>
-            <p className="about-parr">
-              Full Stack web developer with extensive experience in supply chain
-              and manufacturing. Taking the years of experience in meeting
-              deadlines, finding solutions, solving problems and just plain
-              getting things done. And taking the same drive and passion into an
-              exciting new Tech career.{" "}
-            </p>
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col   className="blue light-4 white-text"
+    s={3}>
+              {" "}
+              <p className="education-title">
+                {" "}
+                Full Stack Web Developer (MERN), HTML - CSS - JS - REACT -
+                Express - Node - Python - Django{" "}
+              </p>
+            </Col>
 
-        <div>
+            <Col s={9}>
+              <p className="about-parr">
+                Full Stack web developer with extensive experience in supply
+                chain and manufacturing. Taking the years of experience in
+                meeting deadlines, finding solutions, solving problems and just
+                plain getting things done. And taking the same drive and passion
+                into an exciting new Tech career.{" "}
+              </p>
+            </Col>
+          </Row>
+        <Row>
+          <Col   className="blue light-4 white-text"
+    s={3}>
           <h2 className="education-title">
             Full Stack Web Developer Bootcamp - HyperionDev 2020
           </h2>
+          </Col>
+          <Col s={9}>
           <h3 className="about-parr">
             {" "}
             Field of study: Full Stack web Development with the M.E.R.N. stack{" "}
           </h3>
           <div className="about-parr">
-            <p className="about-parr">
-              Specialization - Web Page, Digital/Multimedia and Information
+            <p className="about-parr">Specialization - Web Page, Digital/Multimedia and Information
               Resources Design
             </p>
           </div>
-        </div>
-
-        <div>
+          </Col>
+          </Row>
+<Row>          <Col   className="blue light-4 white-text"
+    s={3}>
           <h3 className="education-title">
             Programme in Purchasing and Supply Management - UNISA 2015
           </h3>
+          </Col> 
+          <Col s={9}>
           <h3 className="about-parr">
             Field of study: Purchasing, Procurement/Acquisitions and Contracts
             Management{" "}
@@ -65,7 +73,10 @@ const About = () => {
               Specialization - Procurement and Stock Control
             </p>
           </div>
-        </div>
+            </Col>
+            </Row>
+
+        </Container>
       </div>
     </>
   );
